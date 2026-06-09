1f43a482a6394ce2d5c8c234d09ec848d3625809
Med khalil bousnina <mkbousnina@gmail.com>
2026-06-09 23:00:12 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css"
 import ClientLayout from "./ClientLayout"
 
 export const metadata: Metadata = {
-  title: "Euro Legal GPT – Deutsche Rechtsplattform für Studierende",
-  description: "Über 5000 deutsche Rechtsdokumente und KI-Unterstützung für deine Prüfungsvorbereitung.",
+  title: "Morched Legale El Tounsi - مرشد القانوني التونسي",
+  description: "Morched Legale est une plateforme AI sous format chat comme messenger, actualisé avec les documents legales de tunisie, qui vise les étudiants et les professionnels. Des abonnements et differents offres sont disponibles",
 icons: {
 icon: "/favicon.svg",
 },
